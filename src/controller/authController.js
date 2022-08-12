597e99f50f8c632c621ef277d1a1b6b50d5504aa
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-08-12 00:23:36 -0300

    Update authController.js

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post("/register", async (req, res) => {
   try {
-    const user = User.create(req.body);
+    const user = await User.create(req.body);
     return res.send({ user });
   } catch (err) {
     return res.status(400).send({ error: "Falha no registro" });
