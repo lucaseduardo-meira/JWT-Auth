845bf09375d8edda5312e8c6296aa3202b2b614f
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-08-12 00:36:03 -0300

    mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(
+  "mongodb+srv://lucasmeira:<password>@cluster0.ql8xc.mongodb.net/?retryWrites=true&w=majority"
+);
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
