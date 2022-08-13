a7664864b591ef4cbdc95964af8f687a23a748c2
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-08-12 23:02:52 -0300

    new folder

--- src/app_config/controller/authController.js
+++ src/app_config/controller/authController.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const authConfig = require("../config/auth.json");
+const authConfig = require("../../config/auth.json");
 
 const User = require("../models/User");
 
