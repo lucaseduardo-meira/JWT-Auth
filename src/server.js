1ee64c71e41b3259cc59b4426cb73d8417e690df
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-08-12 23:12:37 -0300

    node mailer

--- src/server.js
+++ src/server.js
@@ -6,8 +6,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
+// GET request
 require("./controller/projectController")(app);
+// Authentication
 require("./controller/authController")(app);
 
 app.listen(3000);
