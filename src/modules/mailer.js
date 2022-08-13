16c3286a1d0b80f24a16fbe5fb73724f773c57bf
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-08-12 23:20:32 -0300

    handlebars + mailer

---
+++ src/modules/mailer.js
@@ -0,0 +1,22 @@
+const nodemailer = require("nodemailer");
+const hbs = require("nodemailer-express-handlebars");
+const path = require("path");
+
+const { host, port, user, pass } = require("../config/mail.json");
+
+var transport = nodemailer.createTransport({
+  host,
+  port,
+  auth: { user, pass },
+});
+
+transport.use(
+  "compile",
+  hbs({
+    viewEngine: "handlebars",
+    viewPath: path.resolve("./src/resource/mail"),
+    extName: ".ejs",
+  })
+);
+
+module.exports = transport;
