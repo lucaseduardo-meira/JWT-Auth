a7f0a6a9017d2af8a2a2d56aa9fc86b2a6934ad7
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-08-18 00:43:46 -0300

    trying to fix req.body

--- src/app_config/controller/authController.js
+++ src/app_config/controller/authController.js
@@ -19,11 +19,10 @@ module.exports = {
   async create(req, res) {
     const { email } = req.body;
     try {
-      if (await User.findOne({ email }))
+      if (await User.findOne({ email })) {
         return res.status(400).send({ error: "Usuario já existe" });
+      }
       const user = await User.create(req.body);
-
-      user.password = undefined;
 
       return res.send({ user, token: generateToken({ id: user.id }) });
     } catch (err) {
