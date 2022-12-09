f4488a33c1e083d4eaa090a193ce5f15cd8364f8
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-12-08 23:34:25 -0300

    Revert "deploy"
    This reverts commit e8762c57d9b0574b5a23fa992b826ddba02ba64c.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const mongoose = require("mongoose");
 
 const app = express();
 
@@ -15,15 +14,7 @@ app.use("/css", express.static(path.resolve(__dirname, "views/css")));
 app.use("/", require("./src/router/routes"));
 
 const PORT = process.env.PORT || 3000;
-mongo_uri = process.env.MONGO_URI || "mongodb://localhost/jwtapp";
 
-mongoose.connect(mongo_uri, function (error) {
-  if (error) {
-    return console.log("error", error);
-  }
-  app.listen(PORT, () => {
-    console.log("Server running on http://localhost:3000");
-  });
+app.listen(PORT, () => {
+  console.log("Server running on http://localhost:3000");
 });
-
-module.exports = mongoose;
