1c3ece171c6147d45d7fc1179fef69c22e687827
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-12-09 23:49:22 -0300

    update

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-mongo_uri = process.env.MONGO_URI || "mongodb://localhost/jwtapp";
+mongo_uri = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(mongo_uri);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    // console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
     process.exit(1);
