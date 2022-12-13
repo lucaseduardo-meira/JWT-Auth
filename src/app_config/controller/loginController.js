0fd7b4d44b319b182b8ae76239539911da5eaf64
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-12-12 23:43:42 -0300

    Update loginController.js

--- src/app_config/controller/loginController.js
+++ src/app_config/controller/loginController.js
@@ -3,7 +3,7 @@ const express = require("express");
 const authMiddleware = require("../middlewares/auth");
 const route = express.Router();
 
-const User = require("../models/User");
+const User = require("../models/user");
 
 module.exports = {
   async login(req, res) {
